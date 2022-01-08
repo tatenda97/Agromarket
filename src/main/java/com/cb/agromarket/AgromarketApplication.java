6d9301f3fa4453110c8780949a254b3219cc261b
tatenda97 <tmashinge97@gmail.com>
2022-01-08 10:43:18 +0200

    Initial commit

---
+++ src/main/java/com/cb/agromarket/AgromarketApplication.java
@@ -0,0 +1,13 @@
+package com.cb.agromarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgromarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgromarketApplication.class, args);
+    }
+
+}
